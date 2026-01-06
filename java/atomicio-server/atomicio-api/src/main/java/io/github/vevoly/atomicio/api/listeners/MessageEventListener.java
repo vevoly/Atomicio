e2e0c3a54c7788a185481d6707e6cb0cee4f6c48
vevoly <vevolyang@gmail.com>
2026-01-06 13:16:10 +0800

    feat: 1 对项目结构进行调整

---
+++ java/atomicio-server/atomicio-api/src/main/java/io/github/vevoly/atomicio/api/listeners/MessageEventListener.java
@@ -0,0 +1,21 @@
+package io.github.vevoly.atomicio.api.listeners;
+
+import io.github.vevoly.atomicio.api.AtomicIOSession;
+import io.github.vevoly.atomicio.api.AtomicIOMessage;
+
+/**
+ * 消息事件监听器
+ *
+ * @since 0.0.1
+ * @author vevoly
+ */
+@FunctionalInterface
+public interface MessageEventListener {
+
+    /**
+     * 当收到消息时被调用。
+     * @param session 来源会话
+     * @param message 收到的消息
+     */
+    void onMessage(AtomicIOSession session, AtomicIOMessage message);
+}
---
+++ java/atomicio-server/atomicio-core/src/main/java/io/github/vevoly/atomicio/core/session/NettySession.java
@@ -0,0 +1,99 @@
+package io.github.vevoly.atomicio.core.session;
+
+import io.github.vevoly.atomicio.api.AtomicIOEngine;
+import io.github.vevoly.atomicio.api.AtomicIOSession;
+import io.github.vevoly.atomicio.api.AtomicIOMessage;
+import io.netty.channel.Channel;
+import io.netty.util.AttributeKey;
+
+import java.util.Objects;
+
+/**
+ * AtomicIOSession 的默认实现，基于 Netty Channel。
+ * 这个类是连接我们抽象 API 和 Netty 底层实现的桥梁。
+ *
+ * @since 0.0.1
+ * @author vevoly
+ */
+public class NettySession implements AtomicIOSession {
+
+    /**
+     * Netty 的 Channel 是实际的网络连接管道
+     */
+    private final Channel channel;
+
+    /**
+     * IO 引擎
+     * Session 持有 IO 引擎
+     */
+    private final AtomicIOEngine engine;
+
+    private final long creationTime = System.currentTimeMillis();
+
+    public NettySession(Channel channel, AtomicIOEngine engine) {
+        this.channel = Objects.requireNonNull(channel, "Channel cannot be null");
+        this.engine = Objects.requireNonNull(engine, "Engine cannot be null");
+    }
+
+    @Override
+    public String getId() {
+        // 使用 Netty Channel 的唯一 ID
+        return channel.id().asLongText();
+    }
+
+    @Override
+    public void send(AtomicIOMessage message) {
+        // 直接将消息写入 Channel，ChannelPipeline 中的 Encoder 会负责将其转换为 ByteBuf
+        channel.writeAndFlush(message);
+    }
+
+    @Override
+    public void close() {
+        channel.close();
+    }
+
+    @Override
+    public boolean isActive() {
+        return channel.isActive();
+    }
+
+    @Override
+    public AtomicIOEngine getEngine() {
+        return this.engine;
+    }
+
+    @Override
+    public String getRemoteAddress() {
+        return channel.remoteAddress().toString();
+    }
+
+    @Override
+    public long getCreationTime() {
+        return this.creationTime;
+    }
+
+    @Override
+    public long getLastActivityTime() {
+        // todo 需要配合 Netty 的 IdleStateHandler 或自定义的 Handler 来更新时间戳
+        // 先返回一个简单实现，后续再完善
+        Long lastActivity = getAttribute("lastActivity");
+        return lastActivity != null ? lastActivity : creationTime;
+    }
+
+    @Override
+    public void setAttribute(String key, Object value) {
+        AttributeKey<Object> attributeKey = AttributeKey.valueOf(key);
+        channel.attr(attributeKey).set(value);
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public <T> T getAttribute(String key) {
+        AttributeKey<Object> attrKey = AttributeKey.valueOf(key);
+        return (T) channel.attr(attrKey).get();
+    }
+
+    public Channel getNettyChannel() {
+        return channel;
+    }
+}
